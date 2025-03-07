bf788dd8450b9b60ea3ef8c91052372b337c2f1f
Hou Un(Franky) Chau <houun.chau@gmail.com>
2025-03-07 04:19:23 -0500

    add more buttons, emoji picker modal, select and display an emoji over the image

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,13 +10,13 @@ type Props = {
 export default function Button({ label, theme, onPress }: Props) {
   if (theme === 'primary') {
     return (
-      <View 
+      <View
         style={[
           styles.buttonContainer,
           { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
         ]}>
-        <Pressable 
-          style={[styles.button, { backgroundColor: '#fff' }]} 
+        <Pressable
+          style={[styles.button, { backgroundColor: '#fff' }]}
           onPress={onPress}>
           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
@@ -26,7 +26,7 @@ export default function Button({ label, theme, onPress }: Props) {
   }
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
@@ -42,19 +42,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 3,
   },
-    button: {
-        borderRadius: 10,
-        width: '100%',
-        height: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'row',
-    },
-    buttonIcon: {
-        paddingRight: 8,
-    },
-    buttonLabel: {
-        color: '#fff',
-        fontSize: 16,
-    },
+  button: {
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: '#fff',
+    fontSize: 16,
+  },
 });
